6c56449853832def03482081dfa70a6357dfecc3
Todd Skinner <todd.skinner@gmail.com>
2014-02-04 21:59:47 +0700

    analytics fixed bug

--- app/assets/javascripts/segmentio.js
+++ app/assets/javascripts/segmentio.js
@@ -1,8 +1,7 @@
-<script type="text/javascript"> window.analytics||(window.analytics=[]),window.analytics.methods=["identify","track","trackLink","trackForm","trackClick","trackSubmit","page","pageview","ab","alias","ready","group","on","once","off"],window.analytics.factory=function(t){return function(){var a=Array.prototype.slice.call(arguments);return a.unshift(t),window.analytics.push(a),window.analytics}};for(var i=0;i<window.analytics.methods.length;i++){var method=window.analytics.methods[i];window.analytics[method]=window.analytics.factory(method)}window.analytics.load=function(t){var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=("https:"===document.location.protocol?"https://":"http://")+"d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n)},window.analytics.SNIPPET_VERSION="2.0.8", window.analytics.load("594qsnm51w"); window.analytics.page(); </script>
-
+var analytics=analytics||[];(function(){var e=["identify","track","trackLink","trackForm","trackClick","trackSubmit","page", "pageview","ab","alias","ready","group"],t=function(e){return function(){analytics.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var n=0;n<e.length;n++)analytics[e[n]]=t(e[n])})(),analytics.load=function(e){var t=document.createElement("script");t.type="text/javascript",t.async=!0, t.src=("https:"===document.location.protocol?"https://":"http://")+ "d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/"+e+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n)}; analytics.load("594qsnm51w");
 $(document).on('page:load', function() {
 	console.log('page loaded');
 	analytics.pageview(); 
-	analytics.trackForm($('#new_visitor'), 'Signed Up');
+	analytics.trackForm($('#new_visitor'), 'Signed Up'); 
 	analytics.trackForm($('#new_contact'), 'Contact Request');
 })
